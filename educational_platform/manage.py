1619029de6123838256b1b2111c4218c7a789f86
Chikago13 <sivko2201@gmail.com>
2023-11-07 12:36:19 +0300

    create poetry

--- educational_platform/manage.py
+++ educational_platform/manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+# pylint: skip-file
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educational_platform.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educational_platform.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +20,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- educational_platform/user/models.py
+++ educational_platform/user/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from .manager import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return f"{self.first_name}"
+
+    def __str__(self):
+        return f"{self.email}"
