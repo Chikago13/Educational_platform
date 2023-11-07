9e4ffbb3348f9b711e5246f920b7d85e27d8a8f1
Chikago13 <sivko2201@gmail.com>
2023-11-07 19:31:51 +0300

    create_model_teacher

--- educational_platform/mentorship/models.py
+++ educational_platform/mentorship/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from study.models import Specialization
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
+
+
+
+    def __repr__(self):
+        return f'{self.user}, {self.specialization}'
+
+    def __str__(self):
+        return f'{self.user} {self.specialization}'
+    
+    class Meta:
+        verbose_name = "Учитель"
+        verbose_name_plural = "Учителя"
+        ordering = ["user", "specialization"]
+
