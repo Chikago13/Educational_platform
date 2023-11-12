9f2055bb5fc695039388e43b4f98d4fa2f35022d
Chikago13 <sivko2201@gmail.com>
2023-11-12 16:12:02 +0300

    migrations

---
+++ educational_platform/constant/mixin.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+class DateTimeMixin(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        abstract = True
+
+
