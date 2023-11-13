791fdbc4c1e133cc195b5a4cdc09e962283c47a4
Chikago13 <sivko2201@gmail.com>
2023-11-13 22:17:07 +0300

    create ListAPIView

--- educational_platform/mentorship/serializers.py
+++ educational_platform/mentorship/serializers.py
@@ -22,3 +22,21 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+
+class TeacherStudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teacher
+        field  = '__all__'
+
+    def to_representation(self, instance):
+        if isinstance(instance, Teacher):
+            serializer = TeacherSerializer(instance)
+        elif isinstance(instance, Student):
+            serializer =StudentSerializer(instance)
+        else:
+            raise ValueError()
+        return serializer.data
+
+
--- educational_platform/mentorship/views.py
+++ educational_platform/mentorship/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from itertools import chain
 
 from .models import Teacher, Student, Group
-from .serializers import TeacherSerializer, StudentSerializer, GroupSerializer
+from .serializers import TeacherSerializer, StudentSerializer, GroupSerializer, TeacherStudentSerializer
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
@@ -14,3 +16,10 @@ class StudentViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class StudentTeacherListAPIView(ListAPIView):
+    student = Student.objects.all()
+    teacher =Teacher.objects.all()
+    queryset = chain(student, teacher)
+    serializer_class = TeacherStudentSerializer
