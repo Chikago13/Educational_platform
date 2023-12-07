1f8b72518717cf5b15a0c1df2259583921ed8a33
Chikago13 <sivko2201@gmail.com>
2023-12-07 22:35:01 +0300

    pyjwt

--- educational_platform/user/models.py
+++ educational_platform/user/models.py
@@ -1,8 +1,11 @@
 from constant.mixin import DateTimeMixin
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+import jwt
+from educational_platform.settings import SECRET_KEY
 
 from .manager import UserManager
+from datetime import datetime, timedelta
 
 
 class User(DateTimeMixin, AbstractBaseUser, PermissionsMixin):
@@ -25,3 +28,17 @@ class User(DateTimeMixin, AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f"{self.email}"
+    
+    def _generet_jwt_token(self):
+        dt = datetime.now() + timedelta(days = 1)
+        token = jwt.encode({'id': self.pk, 
+                            'email': self.email, 
+                            'exp': int(dt.strftime('%s'))}, 
+                            SECRET_KEY, 
+                            algorithm = 'HS256')
+        return token
+
+    @property
+    def token(self):
+        return self._generet_jwt_token()
+    
--- educational_platform/user/urls.py
+++ educational_platform/user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .endpoints import LoginAPIView
 from .views import UserViewSet
+
 
 router = DefaultRouter()
 router.register(r"user", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
